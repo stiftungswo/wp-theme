5a058abb1fac2360c1af5654b947e577e5d298ee
claudioge <60546759+dio-git@users.noreply.github.com>
2020-07-03 14:27:56 +0200

    Added Slideshow

--- template-parts/front-header.php
+++ template-parts/front-header.php
@@ -9,5 +9,65 @@
 
 ?>
 
-<img src="http://localhost:8080/wp-content/uploads/2020/06/844198.jpg" alt="IMAGE NOT AVAILABLE">
 
+ <!-- Slideshow container -->
+<div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <img src="http://localhost:8080/wp-content/uploads/2020/06/844198.jpg" style="width:100%">
+  <div class="text">Was ist die SWO?</div>
+</div>
+
+
+<div class="mySlides fade">
+  <img src="http://localhost:8080/wp-content/uploads/2020/06/slemenova-spica-triglav-national-park.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+    <img src="http://localhost:8080/wp-content/uploads/2020/06/slid2.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
