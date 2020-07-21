3d392090cdcc57e60bc9f712f04ba69f8191540e
David Kessler <davidkessler@gmx.ch>
2020-07-21 11:33:21 +0200

    add multiple colums in bereiche template

--- templates/template-bereich.php
+++ templates/template-bereich.php
@@ -19,8 +19,42 @@ get_template_part( 'template-parts/bereich-header' );
 <div class="limit-content-width bereich-container">
 
     <div class="left-right">
-        <div class="bereich-left"></div>
-        <div class="bereich-right classic-text">
+        <div class="bereich-image"></div>
+        <div class="bereich-text classic-text">
+            <h5 class="no-serif-heading">Feld- Zivis</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis qui iste, cupiditate rerum dolorum quo tempore cumque excepturi deleniti tenetur, 
+                culpa ut ducimus ex molestias ipsa sint voluptas laboriosam dolores?</p>
+            <ul class="feature-list">
+                <li><span>&#10003;</span>   Revitalisierung von Fliess- und Stillgewässern</li>
+                <li><span>&#10003;</span>	Begleitung von Schulklassen bei Umwelteinsätzen</li>
+                <li><span>&#10003;</span>	Heckenpflege</li>
+                <li><span>&#10003;</span>	Revitalisierung von Fliess- und Stillgewässern</li>
+            </ul>
+            <br>
+            <a href="#" class="swo-button zivi-braun" style="color: #61330E !important;
+	border: 2px solid #61330E;">Jetzt Feld- Zivi Werden!</a>
+        </div>
+    </div>
+    <div class="left-right">
+        <div class="bereich-text classic-text">
+            <h5 class="no-serif-heading">Feld- Zivis</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis qui iste, cupiditate rerum dolorum quo tempore cumque excepturi deleniti tenetur, 
+                culpa ut ducimus ex molestias ipsa sint voluptas laboriosam dolores?</p>
+            <ul class="feature-list">
+                <li><span>&#10003;</span>   Revitalisierung von Fliess- und Stillgewässern</li>
+                <li><span>&#10003;</span>	Begleitung von Schulklassen bei Umwelteinsätzen</li>
+                <li><span>&#10003;</span>	Heckenpflege</li>
+                <li><span>&#10003;</span>	Revitalisierung von Fliess- und Stillgewässern</li>
+            </ul>
+            <br>
+            <a href="#" class="swo-button zivi-braun" style="color: #61330E !important;
+	border: 2px solid #61330E;">Jetzt Feld- Zivi Werden!</a>
+        </div>
+        <div class="bereich-image"></div>
+    </div>
+    <div class="left-right">
+        <div class="bereich-image"></div>
+        <div class="bereich-text classic-text">
             <h5 class="no-serif-heading">Feld- Zivis</h5>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis qui iste, cupiditate rerum dolorum quo tempore cumque excepturi deleniti tenetur, 
                 culpa ut ducimus ex molestias ipsa sint voluptas laboriosam dolores?</p>
