ae0072d7789fcd320b453baf57de2a7880eec3e5
claudioge <60546759+dio-git@users.noreply.github.com>
2020-07-22 11:33:47 +0200

    New Post template

--- templates/template-inhalt.php
+++ templates/template-inhalt.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Inhaltsseite Template
+ * Template Name: Inhalt Template
  * Template Post Type: post, page
  *
  * @package WordPress
@@ -15,7 +15,6 @@ get_header();
 <?php get_template_part( 'template-parts/entry-header' );?>
 
 <div class="limit-content-width">
-    <div class=content-container>
 
 
         
@@ -23,34 +22,4 @@ get_header();
             <?php the_post();?>
             <?php get_template_part( 'template-parts/content-2' ); ?>
         </div>
-
-
-
-        <div class="side-inhalt">
-                <h5><?php echo get_post_meta( get_the_ID(), 'Projektstart', true ); ?></h5>
-                <p>Projektstart<p>
-
-                <h5><?php echo get_post_meta( get_the_ID(), 'Bereich', true ); ?></h5>
-                <p>Bereich<p>
-
-                <h5><?php echo get_post_meta( get_the_ID(), 'Ort', true ); ?></h5>
-                <p>Ort<p>
-
-                <hr/>
-
-                <p><?php echo get_post_meta( get_the_ID(), 'Hashtags', true ); ?><p/>
-
-                <hr/>
-
-                <h5>Kontakt</h5>
-                <p><?php echo get_post_meta( get_the_ID(), 'Kontakt', true ); ?></p>
-
-        </div>
-
-    </div>
 </div>
-
-
-
-
-<?php get_footer(); ?>
