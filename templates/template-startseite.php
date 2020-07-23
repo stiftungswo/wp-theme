cd18e457bc85eccdcc89f4d03e286834ae21d4a2
PrinzWillyM <siro.duschletta@hispeed.ch>
2020-07-23 17:31:03 +0200

    Dynamic generated Project Cards (Attention, include some changes in the DB and in the wp-admin parts...)

--- templates/template-startseite.php
+++ templates/template-startseite.php
@@ -67,15 +67,57 @@ get_template_part('template-parts/front-header');
 
     <section class="boxes-container-menu">
         <ul>
-
             <?php
-
-            get_template_part( 'template-parts/project-box' );
-            get_template_part( 'template-parts/project-box' );
-            get_template_part( 'template-parts/project-box' );
-            get_template_part( 'template-parts/project-box' );
-
+            $args = array(
+              'post_parent' => 8334 //parent of all "Project-Posts"
+            );
+            //gets all posts with "Project" as parent
+            $children = get_children($args, $output = OBJECT ); 
             ?>
+            <?php foreach($children as $postNr => $postObjectByNr) : ?>
+                <?php
+                //gets the name, description and image-post of the project by meta data
+                $projectName = get_post_meta( $postNr, 'projektname' ); 
+                $projectDescription = get_post_meta( $postNr, 'projektbeschreibung' );
+                $projectImagePostNr = get_post_meta( $postNr, 'projektbild' );
+                //gets permanentLink to the img url from the Post
+                $projectImagePermalink = get_the_guid( $projectImagePostNr[0] );
+                ?>
+
+                <li class="wrap-boxes">
+                <?php 
+                echo '<div class="imageDiv img-background" style="background-image: url(' . $projectImagePermalink . ')" alt="Several hands holding beer glasses"></div>';
+                ?>
+                <div class="imageDiv bottomDiv classic-text">
+                    <h1>
+                        <?php 
+                        //display if not empty
+                        if(empty($projectName[0])) {
+                            echo "Projekt";
+                        }else {
+                            echo $projectName[0];
+                        }
+                        ?>
+                    </h1>
+                    <p>
+                        <?php 
+                        //display if not empty
+                        if(empty($projectDescription[0])) {
+                            echo "Projekt";
+                        } else {
+                            echo $projectDescription[0];
+                        }
+                        ?>
+                    </p>
+                    <?php
+                        //permalink to foward to the specific project page
+                        $postPermalink = get_permalink( $post = $postNr, $leavename = false );
+                        echo '<a class="svg-button" href="' . $postPermalink . '">Weiterlesen</a>';
+                    ?>
+                </div>
+            </li>
+
+            <?php endforeach; ?>
 
         </ul>
     </section>
